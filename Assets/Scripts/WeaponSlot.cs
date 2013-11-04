48cc574407af083c07ce501549e6b308ef9ab714
Matias Juntunen <matias.juntunen@gmail.com>
2013-11-04 04:39:19 +0200

    more shit

--- Assets/Scripts/WeaponSlot.cs
+++ Assets/Scripts/WeaponSlot.cs
@@ -3,42 +3,26 @@ using System.Collections;
 
 public class WeaponSlot : MonoBehaviour {
 
-    public GameObject weaponPrefab;
+    public GameObject defaultWeaponPrefab;
 
-    // Use this for initialization
-    void Start () {
-        var weapon = (GameObject)Instantiate(weaponPrefab, transform.position, transform.rotation);
+    void Start()
+    {
+        SetWeapon(defaultWeaponPrefab);
+    }
+    
+    public void SetWeapon(GameObject WeaponPrefab)
+    {
+        var weapon = (GameObject)Instantiate(WeaponPrefab, transform.position, transform.rotation);
         weapon.transform.parent = transform;
 
         var trigger = weapon.GetComponent<TriggerOnMouseOrJoystick>();
-        var player = GameObject.FindGameObjectWithTag("Player");
+        var player = GameObject.FindGameObjectWithTag("PlayerAnim");
 
-        var mouseDownReceivers = new ReceiverItem[2];
-
-        mouseDownReceivers[0] = new ReceiverItem();
-        mouseDownReceivers[0].action = "OnStartFire";
-        mouseDownReceivers[0].receiver = weapon;
-
-        mouseDownReceivers[1] = new ReceiverItem();
-        mouseDownReceivers[1].action = "OnStartFire";
-        mouseDownReceivers[1].receiver = player;
-
-        var mouseUpReceivers = new ReceiverItem[2];
-
-        mouseUpReceivers[0] = new ReceiverItem();
-        mouseUpReceivers[0].action = "OnStopFire";
-        mouseUpReceivers[0].receiver = weapon;
-
-        mouseUpReceivers[1] = new ReceiverItem();
-        mouseUpReceivers[1].action = "OnStopFire";
-        mouseUpReceivers[1].receiver = player;
-
-        trigger.mouseDownSignals.receivers = mouseDownReceivers;
-        trigger.mouseUpSignals.receivers = mouseUpReceivers;
+        trigger.mouseUpSignals.receivers[1]   = new ReceiverItem() { receiver = player, action = "OnStartFire" };
+        trigger.mouseDownSignals.receivers[1] = new ReceiverItem() { receiver = player, action = "OnStopFire" };
     }
-    
-    // Update is called once per frame
-    void Update () {
-    
+
+    void Update()
+    {
     }
 }
