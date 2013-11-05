0483e58a57ab28549dc0299c6310a636b6d472be
Samppalol <sami.salonen@yahoo.com>
2013-11-06 00:27:06 +0200

    the health bar is now an actual bar

--- Assets/GuiTest.cs
+++ Assets/GuiTest.cs
@@ -7,6 +7,7 @@ public class GuiTest : MonoBehaviour {
 	public Health health;
 	public float healthNow;
 	public float healthMax;
+	public float healthbarLength;
 	
 	// Use this for initialization
 	void Start () {
@@ -14,30 +15,43 @@ public class GuiTest : MonoBehaviour {
 		health = player.GetComponent<Health>();
 		healthNow = health.health;
 		healthMax = health.maxHealth;
+		healthbarLength = healthNow/healthMax;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		healthNow = (int)health.health;
-		//healthMax = health.maxHealth;
+		//170 would fill the entire hp bar "box", I've used a smaller value here so things wouldn't overlap.
+		healthbarLength = (healthNow/healthMax)*166;	
 	}
 
 	void OnGUI () {
-				
-			//if (GUI.Button (new Rect (10,10,200,20), "Meet the flashing button")) {
-			//	print ("You clicked me!");
-			//}
 			
-			//name or sumthin
-			GUI.Label (new Rect (10,369,201,30), "Das Player", "box");
+		// an example of a button for reference
+		//if (GUI.Button (new Rect (10,10,200,20), "Meet the flashing button")) {
+		//	print ("You clicked me!");
+		//}
 		
-			//health
-			GUI.Label (new Rect (10,400,30,30), "HP: ", "box");
-			GUI.Label (new Rect (41,400,170,30), healthNow + " / " + healthMax, "box"); 
-				
-			//ammo etc
-			GUI.Label (new Rect (10,431,50,30), "AMMN: ", "box");
-			GUI.Label (new Rect (61,431,150,30), "999" + " / " + "999", "box");
+		//name or sumthin
+		GUI.Label (new Rect (10,369,201,30), "Das Player", "box");
+			
+		//health
+		GUI.Label (new Rect (10,400,30,30), "HP: ", "box");
+		GUI.Label (new Rect (41,400,170,30), "","box"); 
+			
+		//ammo etc
+		GUI.Label (new Rect (10,431,50,30), "AMMN: ", "box");
+		GUI.Label (new Rect (61,431,150,30), "999" + " / " + "999", "box");
+	
+		if (healthNow > 50) {
+			GUI.color = Color.green;
+		} else if (healthNow > 35) {
+			GUI.color = Color.yellow;
+		} else {
+			GUI.color = Color.red;
+		}
+		
+		GUI.Label(new Rect(43, 402, healthbarLength, 26), healthNow + "", "box");
 		
 	}
 }
