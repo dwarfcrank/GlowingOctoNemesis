c3b2de28be3b2220bc5ba08bce158d8db21c2d21
cobrelli <cobrelli@gmail.com>
2013-11-06 16:52:49 +0200

    picking up spiders ;)

---
+++ Assets/PickupSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupSpawner : MonoBehaviour 
+{
+	
+	public GameObject[] pickups;
+	public int TimeMultiplier;
+	public int MaxWaitWithoutMultiplier;
+	public bool running;
+	public bool active;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	//	SpawnPickup();
+	}
+	
+	//void SpawnPickup()
+	//{
+	//	Debug.Log ("lol");hmm
+	//	yield return new WaitForSeconds(3f);
+	//	
+	//}
+}
---
+++ Assets/SpawnPickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPickup : MonoBehaviour {
+	
+	public GameObject[] pickups;
+	public int numberOfPickups;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnDeath(){
+		if(Random.Range (1,100)<30)
+		{
+			transform.DetachChildren();
+			int pickup = Random.Range(0,numberOfPickups);
+			Vector3 pos = new Vector3(transform.position.x, transform.position.y+1, transform.position.z);
+			Quaternion rot = new Quaternion(0,90,90,0);
+			Instantiate(pickups[pickup], pos, rot);
+		}
+	}
+}
