a75725f9d9e07cf994ded7b93503751b5780d058
Matias Juntunen <matias.juntunen@gmail.com>
2013-11-06 06:25:34 +0200

    cool shit

---
+++ Assets/GameEnv.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEnv : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (var enemy in enemies)
+        {
+            enemy.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/GuiTest.cs
+++ Assets/GuiTest.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class GuiTest : MonoBehaviour {
-	
-	public GameObject player;
-	public Health health;
-	public float healthNow;
-	public float healthMax;
-	public float healthbarLength;
+
+    public GUIStyle style;
+
+	private GameObject player;
+	private Health health;
+	private float healthNow;
+	private float healthMax;
+	private float healthbarLength;
 	
 	// Use this for initialization
 	void Start () {
@@ -32,26 +34,28 @@ public class GuiTest : MonoBehaviour {
 		//	print ("You clicked me!");
 		//}
 		
-		//name or sumthin
-		GUI.Label (new Rect (10,369,201,30), "Das Player", "box");
-			
-		//health
-		GUI.Label (new Rect (10,400,30,30), "HP: ", "box");
-		GUI.Label (new Rect (41,400,170,30), "","box"); 
-			
+        var healthXoffset = 10;
+        var healthYoffset = Screen.height - 30 - 10;
+        var ammoXoffset = Screen.width - 201 - 10;
+        var ammoYoffset = healthYoffset;
+        
 		//ammo etc
-		GUI.Label (new Rect (10,431,50,30), "AMMN: ", "box");
-		GUI.Label (new Rect (61,431,150,30), "999" + " / " + "999", "box");
+		GUI.Label (new Rect (ammoXoffset, ammoYoffset - 30, 201, 30), "weapon", "box");
+		GUI.Label (new Rect(ammoXoffset, ammoYoffset, 50, 30), "AMMO ", "box");
+		GUI.Label (new Rect(ammoXoffset + 50, ammoYoffset, 150, 30), "999" + " / " + "999", "box");
 	
 		if (healthNow > 50) {
-			GUI.color = Color.green;
+			GUI.backgroundColor = Color.green;
 		} else if (healthNow > 35) {
-			GUI.color = Color.yellow;
+			GUI.backgroundColor = Color.yellow;
 		} else {
-			GUI.color = Color.red;
+			GUI.backgroundColor = Color.red;
 		}
 		
-		GUI.Label(new Rect(43, 402, healthbarLength, 26), healthNow + "", "box");
+		//health
+		GUI.Label (new Rect (healthXoffset, healthYoffset, 30, 30), "HP", "box");
+		GUI.Label (new Rect (healthXoffset + 30 + 1, healthYoffset, 170,30), "","box");
+		GUI.Label(new Rect(healthXoffset + 30 + 3, healthYoffset + 2, healthbarLength, 26), healthNow + "", "box");
 		
 	}
 }
