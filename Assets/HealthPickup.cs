2696ab643f41f61c420a1ececbb0b02e7d5c3e44
cobrelli <cobrelli@gmail.com>
2013-11-06 07:36:26 +0200

    master of PUA ;F

---
+++ Assets/HealthPickup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickup : Pickup {
+	
+	// Use this for initialization
+	protected override void Start()
+	{
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update()
+    {
+        base.Update();
+    }
+
+	    public override void ApplyEffect(GameObject player)
+    {
+        player.GetComponent<Health>().addHealth(50);
+		Destroy(transform.parent.gameObject);
+    }
+	
+}
+
