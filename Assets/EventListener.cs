1c7627910dfb96d35674cc1f550b3244e54b1892
Tomi Virtanen <cobrelli@gmail.com>
2013-12-06 10:51:43 +0200

    pausesysdeemid

---
+++ Assets/EventListener.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventListener : MonoBehaviour {
+
+	private bool gamePaused;
+
+	// Use this for initialization
+	void Start () {
+		gamePaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("p")) 
+		{
+			//PauseGame();
+			gamePaused = !gamePaused;
+			if (gamePaused) {
+				PauseGame();
+			}else{
+				UnpauseGame();
+			}
+		}
+
+
+	}
+
+	void PauseGame() {
+		Time.timeScale = 0;
+	}
+
+	void UnpauseGame(){
+		Time.timeScale = 1;
+	}
+}
