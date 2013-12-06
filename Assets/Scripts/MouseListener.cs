28ed1ff2c10260f37939dcebddaa31e190616c0a
Tomi Virtanen <cobrelli@gmail.com>
2013-12-06 16:04:56 +0200

    alkumenyyyyyyy :DDDenyyyy :DD desudesu

---
+++ Assets/Scripts/MouseListener.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseListener : MonoBehaviour {
+
+	public bool isQuitButton = false;
+
+	void OnMouseEnter()
+	{
+		Debug.Log("jeeee hiiri tuli");
+		renderer.material.color = Color.green;
+	}
+
+	void OnMouseExit()
+	{
+		Debug.Log("hiiri meni :(");
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUp()
+	{
+		if (isQuitButton) {
+			Application.Quit ();
+		} else {
+			Application.LoadLevel("arena");
+		}
+	}
+}
